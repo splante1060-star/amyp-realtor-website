59042e6bbb0d2d975b563288c938b09163c8f4ae
splante1060-star <splante1060@gmail.com>
2026-08-19 14:07:54 -0400

    fix: comment out closing statement of divorce page

--- src/pages/DivorceRealEstatePage.tsx
+++ src/pages/DivorceRealEstatePage.tsx
@@ -7,7 +7,7 @@ import DivorceVestaSection from "../components/divorce-real-estate/DivorceVestaS
 import DivorceWorkingWithAmySection from "../components/divorce-real-estate/DivorceWorkingWithAmySection";
 import DivorceGuideSection from "../components/divorce-real-estate/DivorceGuideSection";
 import DivorceTestimonialSection from "../components/divorce-real-estate/DivorceTestimonialSection";
-import DivorceClosingStatement from "../components/divorce-real-estate/DivorceClosingStatement";
+// import DivorceClosingStatement from "../components/divorce-real-estate/DivorceClosingStatement";
 
 export default function DivorceRealEstatePage() {
   return (
