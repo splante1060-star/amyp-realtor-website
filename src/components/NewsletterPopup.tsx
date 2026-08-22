86ad4877224d0db46913f44672214b7193b55f16
splante1060-star <splante1060@gmail.com>
2026-08-22 16:07:27 -0400

    fix: delay added to newsletter popup

--- src/components/NewsletterPopup.tsx
+++ src/components/NewsletterPopup.tsx
@@ -13,11 +13,15 @@ declare global {
 
 export default function NewsletterPopup() {
   useEffect(() => {
-    if (typeof window.fd === "function") {
-      window.fd("form", {
-        formId: "675b30b8170efb5ad611c2da",
-      });
-    }
+    const timer = window.setTimeout(() => {
+      if (typeof window.fd === "function") {
+        window.fd("form", {
+          formId: "675b30b8170efb5ad611c2da",
+        });
+      }
+    }, 6000);
+
+    return () => window.clearTimeout(timer);
   }, []);
 
   return null;
